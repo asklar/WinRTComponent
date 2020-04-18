375c4838eb1583d2f0fe4a05ed230a6e539133f0
Alexander Sklar <asklar@microsoft.com>
2020-04-17 22:47:19 -0700

    repro

---
+++ BlankApp1/MainPage.cpp
@@ -0,0 +1,32 @@
+﻿#include "pch.h"
+#include "MainPage.h"
+#include "MainPage.g.cpp"
+#include "Generated Files/winrt/RuntimeComponent1.h"
+
+using namespace winrt;
+using namespace Windows::UI::Xaml;
+
+namespace winrt::BlankApp1::implementation
+{
+    MainPage::MainPage()
+    {
+        InitializeComponent();
+
+        RuntimeComponent1::Class1::Foo();
+    }
+
+    int32_t MainPage::MyProperty()
+    {
+        throw hresult_not_implemented();
+    }
+
+    void MainPage::MyProperty(int32_t /* value */)
+    {
+        throw hresult_not_implemented();
+    }
+
+    void MainPage::ClickHandler(IInspectable const&, RoutedEventArgs const&)
+    {
+        myButton().Content(box_value(L"Clicked"));
+    }
+}
---
+++ Class1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RuntimeComponent1
+{
+    public sealed class Class1
+    {
+        public static void Foo() { }
+    }
+}
